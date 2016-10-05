bb3d972cdadc54826477205f949ffcba0d3066f5
David Bergeron <sceptredata@gmail.com>
2016-10-04 19:46:39 -0600

    Organized it a bit. Need to add more asserts.

---
+++ dTest.c
@@ -0,0 +1,119 @@
+#include "dTest.h"
+
+/* Common strings */
+static const char PASS_STRING[]         = "PASS";
+static const char FAIL_STRING[]         = "FAIL";
+static const char LINE_BREAK_STRING[]   = "--------------------";
+
+/* These EXIT macros save us when our tests fail.*/
+#define TEST_IS_SAFE() (setjmp(Results.exit_point) == 0)
+
+#define FAIL_AND_GOTO_EXIT                    \
+    {Results.active_test_failed = 1;      \
+     longjmp(Results.exit_point, 1);}
+
+/* Forward Output Functions/declarations. */
+#define PRINT_EOL()     putchar('\n')
+void PrintTestStatus(void);
+
+
+/* We store all of our pass/fail results and exit info inside this singleton */
+struct _testResults
+{
+    const char *active_file;
+    const char *active_test;
+    int cur_line;
+    int num_tests;
+    int num_success_tests;
+    int num_failed_tests;
+    int active_test_failed;
+    jmp_buf exit_point;
+};
+struct _testResults Results;
+
+
+/*
+ *  dTest Suite Major Functions
+ */
+
+void InitializeTesting (const char* filename);
+{
+    Results.active_file = filename;
+    Results.active_test = NULL;
+    Results.cur_line_num = 0;
+    Results.num_tests = 0;
+    Results.num_success_tests = 0;
+    Results.num_failed_tests = 0;
+    Results.actve_test_failed = 0;
+}
+
+int EndTesting(void)
+{
+    PRINT_EOL();
+    printf("%s\n", LINE_BREAK_STRING);
+    printf("Tests: %d | Fails: %d\n", Results.num_tests, Results.num_failed_tests);
+    if( Results.num_failed_tests == 0 ){
+        printf("Passed all tests.");
+    } else {
+        printf("FAIL");
+    }
+    PRINT_EOL();
+    fflush(stdout);
+    return Results.num_failed_tests;
+}
+
+void EndActiveTest(void)
+{
+    if (Results.active_test_failed){
+        Results.num_failed_tests++;
+    } else {
+        PrintTestStatus();
+        printf("%s", PASS_STRING);
+    }
+    Results.active_test_failed = 0;
+    PRINT_EOL();
+    fflush(stdout);
+}
+
+/* The ASSERTs inside your Test function take care of reporting failure */
+void RunTest (testFunction_t Func, const char *func_name, const int func_line_num)
+{
+    Results.active_test = func_name;
+    Results.cur_line_num = func_line_num;
+    Results.num_tests++;
+    /* If we haven't ran our test function yet, Do it! */
+    if (TEST_IS_SAFE()){
+        Func();
+    }
+    EndActiveTest();
+}
+
+void FailTest(const char *str, int line_num)
+{
+    if (Results.active_test_failed)
+        return;
+
+    PrintTestStatus(Results.active_file, line_num);
+    printf("%s", FAIL_STRING);
+    if (str != NULL){
+        printf(": %s", str)
+    }
+    FAIL_AND_GOTO_EXIT;
+}
+
+/*
+ * Assert Functions
+ * See macros in dTest.h, they will be easier to use.
+ */
+
+
+/*
+ * Output Functions
+ */
+
+void PrintTestStatus(void)
+{
+    if (Results.active_file != NULL){
+        printf("%s: %d : %s : ", Results.active_file, Results.cur_line_num, Results.active_test);
+    }
+}
---
+++ dTest.h
@@ -0,0 +1,70 @@
+#ifndef DTEST_H
+#define DTEST_H
+
+#include <stdio.h>
+#include <setjmp.h>
+
+/* In case there are still trolls who shun C99 */
+#ifndef DTEST_DISABLE_STDBOOL
+#include <stdbool.h>
+#else
+typedef enum {false = 0, true = !false} bool;
+#endif
+
+/* Function Pointer typedef */
+typedef void (*testFunc_p)(void);
+
+/* Forward function declarations. I reccomend you use the macros just below instead. */
+void InitializeTesting (const char* filename);
+int EndTesting (void);
+
+void RunTest(testFunc_p Func, const char *func_name, const int func_line_num);
+void FailTest(const char *msg, const int line_num);
+
+/* *******************************************************************
+ *  dTest Suite Functions
+ *  Make sure to use START_TESTING() before you try to RUN_TEST
+ *  The function you pass to RUN_TEST should contain your ASSERTS.
+ *  Ex: 
+ *      void TestAssertFunction(void)
+ *       {
+ *           TEST_ASSERT(1 == 1)
+ *       }
+ *       int main(void)
+ *       {
+ *           START_TESTING();
+ *           RUN_TEST(TestAssertFunction);
+ *           return END_TESTING();
+ *       }
+ *  Try to use the macros below, they pass in more information and are 
+ *  more succinct.
+ *********************************************************************/
+
+#define START_TESTING() InitializeTesting(__FILE__)
+#define END_TESTING() EndTesting()
+#define RUN_TEST (func) RunTest(func, #func, __LINE__)
+
+/***********************
+ *  dTest ASSERT Macros *
+ ***********************/
+
+/* Simple Boolean */
+#define TEST_ASSERT_MSG(condition, msg)                 \
+    if((condition) != true) FailTest(msg, __LINE__)
+#define TEST_ASSERT_IS_FALSE_MSG(condition, msg)        \
+    if((condition) != false) FailTest(msg, __LINE__)
+#define TEST_ASSERT_NULL_MSG(p, msg)                    \
+    TEST_ASSERT_MSG(((p) == NULL), msg)
+#define TEST_ASSERT_NOT_NULL_MSG(p, msg)                \
+    TEST_ASSERT_MSG(((p) != NULL), msg)
+
+#define TEST_ASSERT(condition)                          \
+    TEST_ASSERT_MSG((condition), "Expression evaluated to FALSE")
+#define TEST_ASSERT_IS_FALSE(condition)                 \
+    TEST_ASSERT_IS_FALSE_MSG(condition, "Expression evaluated to TRUE")
+#define TEST_ASSERT_NULL(p)                             \
+    TEST_ASSERT_NULL_MSG((p), "Pointer is not NULL")
+#define TEST_ASSERT_NOT_NULL(p)                         \
+    TEST_ASSERT_NOT_NULL_MSG((p), "Pointer is NULL")
+
+#endif
